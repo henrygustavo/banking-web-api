3c91afeafb6a31451739c3724cba28a457bcc1c0
henrygustavo <henrygustavof@gmail.com>
2018-06-18 00:51:41 -0500

    ini

---
+++ Banking.Api/Controllers/Customers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banking.Api.Controllers.Customers
+{
+    [Produces("application/json")]
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+    }
+}
