139836d9fcf55e69f8b68c25142a22ec29e0fadf
Henry <henrygustavof@gmail.com>
2018-06-18 20:29:31 -0500

    Adding Banking.Domain.Test

---
+++ Banking.Domain.Test/Service/TransferDomainServiceTest.cs
@@ -0,0 +1,13 @@
+namespace Banking.Domain.Test
+{
+    using Xunit;
+
+    public class TransferDomainServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
