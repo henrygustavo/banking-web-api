56577103a5880738f626ba9123ae4efa99c5f87f
henrygustavo <henrygustavof@gmail.com>
2018-06-21 00:40:43 -0500

    Adding migration

---
+++ Banking.Infrastructure/Repository/Common/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Banking.Infrastructure.Repository.Common
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BankingContext>
+
+    {
+        public BankingContext CreateDbContext(string[] args)
+        {
+        
+            IConfigurationRoot configuration = new ConfigurationBuilder().Build();
+            var builder = new DbContextOptionsBuilder<BankingContext>();
+            var connectionString = ("Server = (localdb)\\MSSQLLocalDB; Database = BankingDB; Integrated Security = SSPI; MultipleActiveResultSets = true");
+            builder.UseSqlServer(connectionString);
+            return new BankingContext(builder.Options);
+        }
+    }
+}
