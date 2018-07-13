fd2d9d8f1d74e46d5b6a8b78b0b9748802454a76
henrygustavo <henrygustavof@gmail.com>
2018-07-13 00:21:57 -0500

    add unit test

--- Banking.Domain.Test/Service/TransferDomainServiceTest.cs
+++ Banking.Domain.Test/Service/TransferDomainServiceTest.cs
@@ -1,13 +1,18 @@
 namespace Banking.Domain.Test
 {
+    using Banking.Domain.Service.Transactions;
+    using System;
     using Xunit;
 
     public class TransferDomainServiceTest
     {
         [Fact]
-        public void Test1()
-        {
 
+        public void TestPerformTransfer()
+        {
+            TransferDomainService transferDomainService = new TransferDomainService();
+
+            Assert.Throws<ArgumentException>(() => transferDomainService.PerformTransfer(null, null, 0));
         }
     }
 }
